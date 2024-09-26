fddc37f34bf50d9156cfd16b07ca05644f97b58a
Maryam <maryammanjra@gmail.com>
2024-09-26 12:12:46 -0400

    Commit check

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -20,5 +20,7 @@ public class AddressBook {
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
         addressBook.removeBuddy(buddy);
+        System.out.println("addressBook");
+
     }
 }
